eab8779946c2ccd4b6c4c91ff085e8de709ccebb
attietesta <attietesta@gmail.com>
2026-01-29 14:06:22 -0300

    transiciones y sfx

--- ellephancy-ggj-2026/escenas/main_menu.gd
+++ ellephancy-ggj-2026/escenas/main_menu.gd
@@ -10,7 +10,8 @@ func _on_boton_custom_comenzar_pressed() -> void:
 	$FmodEventEmitter2D2.play()
 	$ColorRect2.visible = true
 	var tween = create_tween()
-	tween.tween_property($ColorRect2, "color", Color(0,0,0,1), 1.5)
-	tween.tween_property($FmodEventEmitter2D, "volume", 0.0, 1.5)
-	await get_tree().create_timer(1.5).timeout
+	var tween_audio = create_tween()
+	tween.tween_property($ColorRect2, "color", Color(0,0,0,1), 2)
+	tween_audio.tween_property($FmodEventEmitter2D, "volume", 0.0, 1).set_trans(Tween.TRANS_LINEAR)
+	await get_tree().create_timer(3).timeout
 	get_tree().change_scene_to_file("res://escenas/levels/first_level.tscn")
