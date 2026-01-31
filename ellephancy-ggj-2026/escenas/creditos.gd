fee6a2184beb4ee999cf5c817681e69900dd057d
attietesta <attietesta@gmail.com>
2026-01-30 21:42:44 -0300

    animacion menu principal

--- ellephancy-ggj-2026/escenas/creditos.gd
+++ ellephancy-ggj-2026/escenas/creditos.gd
@@ -6,10 +6,16 @@ func _ready() -> void:
 	$FmodEventEmitter2D.play()
 	$FmodEventEmitter2D.volume = 0.0
 	$ColorRect2.visible = true
-	var tween = create_tween()
+	$AnimationPlayer.play("fade_in")
 	var tween_audio = create_tween()
 	tween_audio.tween_property($FmodEventEmitter2D, "volume", volumen_maximo, 8).set_trans(Tween.TRANS_SINE)
-	tween.tween_property($ColorRect2, "color", Color(0,0,0,0), 5)
+
 
 func _physics_process(delta: float) -> void:
 	$RichTextLabel.position.y -= 40 * delta
+
+
+func _on_boton_custom_pressed() -> void:
+	$AnimationPlayer.play("fade_out")
+	await get_tree().create_timer(3.5).timeout
+	get_tree().change_scene_to_file("res://escenas/main_menu.tscn")
--- ellephancy-ggj-2026/escenas/main_menu.gd
+++ ellephancy-ggj-2026/escenas/main_menu.gd
@@ -14,22 +14,13 @@ func _ready() -> void:
 
 
 func _on_boton_custom_comenzar_pressed() -> void:
-	$FmodEventEmitter2D2.play()
-	$ColorRect2.visible = true
-	var tween = create_tween()
-	var tween_audio = create_tween()
-	tween.tween_property($ColorRect2, "color", Color(0,0,0,1), 2)
-	tween_audio.tween_property($FmodEventEmitter2D, "volume", 0.0, 1).set_trans(Tween.TRANS_LINEAR)
+	$AnimationPlayer.play("fade_out")
 	await get_tree().create_timer(3).timeout
 	get_tree().change_scene_to_file("res://escenas/levels/juego.tscn")
 
 
 func _on_boton_custom_creditos_pressed() -> void:
 	$FmodEventEmitter2D2.play()
-	$ColorRect2.visible = true
-	var tween = create_tween()
-	var tween_audio = create_tween()
-	tween.tween_property($ColorRect2, "color", Color(0,0,0,1), 2)
-	tween_audio.tween_property($FmodEventEmitter2D, "volume", 0.0, 1).set_trans(Tween.TRANS_LINEAR)
+	$AnimationPlayer.play("fade_out")
 	await get_tree().create_timer(3).timeout
 	get_tree().change_scene_to_file("res://escenas/creditos.tscn")
