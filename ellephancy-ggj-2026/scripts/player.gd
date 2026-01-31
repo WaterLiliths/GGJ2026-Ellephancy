2200fbf61453c6d490dda85e3b3aa76a825d3ff6
Marcos Nicolas Salinas <smarcosnicolas@gmail.com>
2026-01-31 04:44:38 -0300

    animaciones arregladas

--- ellephancy-ggj-2026/scripts/player.gd
+++ ellephancy-ggj-2026/scripts/player.gd
@@ -61,14 +61,17 @@ func _input(event: InputEvent) -> void:
 		mascara_tiempo.usar()
 		mascara_fuerza.desactivar()
 		mascara_traducciones.desactivar()
+		verificar_animacion_con_mascara()
 	if Input.is_action_just_pressed("2"): #usar mascara fuerza
 		mascara_tiempo.desactivar()
 		mascara_fuerza.usar()
 		mascara_traducciones.desactivar()
+		verificar_animacion_con_mascara()
 	if Input.is_action_just_pressed("3"): #usar mascara traducciones
 		mascara_tiempo.desactivar()
 		mascara_fuerza.desactivar()
 		mascara_traducciones.usar()
+		verificar_animacion_con_mascara()
 
 	if Input.is_action_just_pressed("tirar") and objeto_arrastrado and Global.mascara_activa==2:
 		conectar_caja_con_joint()
@@ -436,3 +439,20 @@ func _on_animated_sprite_pj_animation_finished() -> void:
 		ejecutar_animacion_caida()
 	if animacion == "agarrar_oso" and estado_actual == ESTADOS.AGARRAR:
 		animated_sprite_pj.play("seguir_agarrando") #TODO TESTEAR
+
+
+func verificar_animacion_con_mascara():
+	var animacion_actual = animated_sprite_pj.get_animation()
+	#agarro la misma animacion q se estaba ejecutando pero como ahora cambio de mascara la mando a ejecutar de nuevo
+	if animacion_actual.begins_with("idle"):
+		ejecutar_animacion_idle()
+	if animacion_actual.begins_with("palanca"):
+		ejecutar_animacion_palanca()
+	if animacion_actual.begins_with("caminar"):
+		ejecutar_animacion_caminar()
+	if animacion_actual.begins_with("salto"):
+		ejecutar_animacion_saltar()
+	if animacion_actual.begins_with("caida"):
+		ejecutar_animacion_caida()
+	if animacion_actual.begins_with("seguir"):
+		ejecutar_animacion_arrastrar()
