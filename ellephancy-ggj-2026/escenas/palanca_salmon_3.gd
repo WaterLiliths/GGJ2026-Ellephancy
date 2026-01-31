95013d08be59b29e17d734c0f3c22b2842b9c0ad
Marcos Nicolas Salinas <smarcosnicolas@gmail.com>
2026-01-31 13:13:03 -0300

    CAMBIOS SALMON

--- ellephancy-ggj-2026/escenas/palanca_salmon_3.gd
+++ ellephancy-ggj-2026/escenas/palanca_salmon_3.gd
@@ -5,7 +5,8 @@ var player_adentro : bool = false
 @export var id_palanca : int = 3
 signal activar_cambiar_runa(id_palanca_int : int)
 @onready var controlador_runas: Node2D = %PuzzleSalmon1
-
+@onready var animation_player_3: AnimationPlayer = %AnimationPlayer3
+var usada: bool = false
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
@@ -20,3 +21,8 @@ func _on_body_exited(body: Node2D) -> void:
 func _physics_process(delta: float) -> void:
 	if player_adentro and Input.is_action_just_pressed("interactuar"):
 		activar_cambiar_runa.emit(id_palanca)
+		if usada:
+			animation_player_3.play("activar")
+		else:
+			animation_player_3.play("desactivar")
+		usada != usada
