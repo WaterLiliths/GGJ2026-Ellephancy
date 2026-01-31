ed8ce36a11fba84bc30ce07bcf29f0829646f6ea
WaterLiliths <waterliliths@gmail.com>
2026-01-30 22:21:23 -0300

    Parallax Presente + Mascaras
    Habemus parallax del presente. Empecé a trabajar en las máscaras, al inicio están todas desactivadas

--- ellephancy-ggj-2026/scripts/global.gd
+++ ellephancy-ggj-2026/scripts/global.gd
@@ -6,7 +6,11 @@ signal mascara_fuerza_activa
 signal mascara_traducciones_desactivar
 signal mascara_tiempo_desactivar
 signal mascara_fuerza_desactivar
-#booleano por mascara
+
+#Booleano por mascara
+var tiene_mascara_fuerza : bool = false
+var tiene_mascara_tiempo : bool = false
+var tiene_mascara_traducciones : bool = false
 
 var mascara_activa : int = 0
 #----------------PALANCA Y PUERTA------------------
