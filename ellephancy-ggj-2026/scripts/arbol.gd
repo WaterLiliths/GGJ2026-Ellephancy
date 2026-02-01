376b0debeb22421d3bdb8f1ce74e9c4772772efa
WaterLiliths <waterliliths@gmail.com>
2026-02-01 03:35:16 -0300

    Avances nivel 4 nivel tortuga porque tengo sueño

--- ellephancy-ggj-2026/scripts/arbol.gd
+++ ellephancy-ggj-2026/scripts/arbol.gd
@@ -1,21 +1,25 @@
+#class_name ObjetoEmpujable 
 extends StaticBody2D
 
-
 @export var presente : bool
+@onready var colision: CollisionPolygon2D = $CollisionPolygon2D
 
+@onready var ultima_posicion : Vector2
 
 func _ready() -> void:
+	ultima_posicion = global_position
 	Global.mascara_tiempo_activa.connect(on_mascara_tiempo_activa)
 	Global.mascara_tiempo_desactivar.connect(on_mascara_tiempo_desactivada)
 	if not presente:
 		esconder_mundo()
 
 
+#-------------FUNCIONES-------------
 func on_mascara_tiempo_activa():
 	if presente:
 		esconder_mundo()
 	else:
-		mostrar_mundo()
+			mostrar_mundo()
 
 
 func on_mascara_tiempo_desactivada():
@@ -26,8 +30,11 @@ func on_mascara_tiempo_desactivada():
 		esconder_mundo()
 
 func esconder_mundo():
+	colision.set_deferred("disabled", true)
 	hide()
 
 
 func mostrar_mundo():
+	global_position = ultima_posicion
+	colision.set_deferred("disabled", false)
 	show()
