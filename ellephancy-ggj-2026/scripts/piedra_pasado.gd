385dc305db5d1af5ee879c3d9f0a5c2787622600
WaterLiliths <waterliliths@gmail.com>
2026-02-01 04:31:08 -0300

    Nivel 4 casi terminado

--- ellephancy-ggj-2026/scripts/piedra_pasado.gd
+++ ellephancy-ggj-2026/scripts/piedra_pasado.gd
@@ -1,12 +1,9 @@
-extends RigidBody2D
+extends StaticBody2D
 
 @export var presente : bool
-var densidad = 1
-@onready var area
-@onready var colision = %CollisionShape2D
+@onready var colision: CollisionPolygon2D = $CollisionPolygon2D
+
 @onready var ultima_posicion : Vector2
-var estaba_en_el_piso : bool = false
-var esta_en_el_aire = false
 
 func _ready() -> void:
 	ultima_posicion = global_position
@@ -14,12 +11,6 @@ func _ready() -> void:
 	Global.mascara_tiempo_desactivar.connect(on_mascara_tiempo_desactivada)
 	if not presente:
 		esconder_mundo()
-	
-	area = $CollisionShape2D.global_scale.x * $CollisionShape2D.global_scale.y
-	mass = 1 + (densidad * area)
-	print(area)
-	print("la masa de esta piedra es " + str(mass))
-	#esta_en_el_aire = $RayCast2D.is_colliding()
 
 
 #-------------FUNCIONES-------------
@@ -46,16 +37,3 @@ func mostrar_mundo():
 	global_position = ultima_posicion
 	colision.set_deferred("disabled", false)
 	show()
-
-
-
-#func _physics_process(_delta: float) -> void:
-	#if velocity.y == 0
-		#$FmodEventEmitter2D2.play_one_shot()
-	##var esta_en_el_piso = %RayCastAbajo.is_colliding()
-	##detectar_caida()
-	
-
-#func detectar_caida():
-	#if esta_en_el_aire and $RayCast2D.is_colliding():
-		#$FmodEventEmitter2D.play_one_shot()
