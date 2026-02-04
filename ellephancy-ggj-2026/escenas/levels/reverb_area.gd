7bdb9fdb4b44d58a4304776699275fe48e3abb0d
attietesta <attietesta@gmail.com>
2026-02-04 11:43:46 -0300

    zonas de reverb

---
+++ ellephancy-ggj-2026/escenas/levels/reverb_area.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		%FmodEventEmitter2D.play()
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body is Player:
+		%FmodEventEmitter2D.stop()
