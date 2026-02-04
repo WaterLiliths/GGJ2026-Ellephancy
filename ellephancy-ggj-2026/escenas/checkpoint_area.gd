640137e3ba9f7d75a6d82c93f8839724d0ef24f0
Marcos Nicolas Salinas <smarcosnicolas@gmail.com>
2026-02-04 16:04:28 -0300

    particulas checkpoint

--- ellephancy-ggj-2026/escenas/checkpoint_area.gd
+++ ellephancy-ggj-2026/escenas/checkpoint_area.gd
@@ -1,11 +1,29 @@
 extends Node2D
 
+@onready var textura_luz: Sprite2D = %TexturaLuz
+
 
 func _ready() -> void:
-	pass # Replace with function body.
+	%CPUParticles2D.emitting = false
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
 		Global.set_checkpoint_position(global_position)
 		$FmodEventEmitter2D.play()
+		%CPUParticles2D.emitting = true
+		tween_entrada()
+		#tween_salida()
+
+
+func tween_entrada():
+	var tween := create_tween()
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(textura_luz,"modulate:a", 1.0, 1.5)
+
+func tween_salida():
+	var tween := create_tween()
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(textura_luz,"modulate:a", 0.0 , 1)
