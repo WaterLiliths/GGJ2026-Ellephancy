ddb396fd6dcda38c44c2978f99332dee64f2a6de
attietesta <attietesta@gmail.com>
2026-02-17 20:04:22 -0300

    sonidos fix

--- ellephancy-ggj-2026/escenas/world_objects/campana_colgante.gd
+++ ellephancy-ggj-2026/escenas/world_objects/campana_colgante.gd
@@ -4,6 +4,7 @@ extends Node2D
 const SECCION_SOGA_CAMPANA = preload("uid://j1ggk4cfsncx")
 const CAMPANA = preload("uid://bnxsiuckubi5t")
 
+@export_enum("Db", "Eb", "F", "Ab", "Bb") var nota : String = "Eb"
 @onready var ancla: RigidBody2D = $Ancla
 @export var longitud_soga : int = 5
 
@@ -26,9 +27,10 @@ func _ready() -> void:
 		anterior = soga
 		if n == longitud_soga - 1:
 			var campana = CAMPANA.instantiate()
-			var campana_joint : PinJoint2D = campana.get_node("PinJoint2D")
+			var campana_joint : PinJoint2D = campana.get_node("PinJointSoga")
 			anterior.add_child(campana)
 			campana.position.y = 80
+			campana.sonido_campana.set_parameter("nota", nota)
 			campana_joint.node_b = anterior.get_path()
 			campana_joint.node_a = campana.get_path()
 			campana_colgante = campana
