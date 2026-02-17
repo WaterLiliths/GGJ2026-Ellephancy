b4b31c41919f957a04d510ce8d3ce017971c4662
attietesta <attietesta@gmail.com>
2026-02-17 12:36:25 -0300

    puzzle secundario nivel 1 y sfx piedra

--- ellephancy-ggj-2026/scripts/objeto_a_empujar.gd
+++ ellephancy-ggj-2026/scripts/objeto_a_empujar.gd
@@ -6,9 +6,13 @@ extends CharacterBody2D
 var direccion : int = 0
 var velocidad : float = 0.0
 var siendo_agarrada : bool = false
+@onready var impacto: FmodEventEmitter2D = $Impacto
+@onready var arrastrar: FmodEventEmitter2D = $Arrastrar
+var sonido_caja_sonando = false
 
 
 func _ready() -> void:
+	impacto.set_parameter("peso", 5.0)
 	ultima_posicion = global_position
 	Global.mascara_tiempo_activa.connect(on_mascara_tiempo_activa)
 	Global.mascara_tiempo_desactivar.connect(on_mascara_tiempo_desactivada)
@@ -19,13 +23,13 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
+		if velocity.y > 0:
+			impacto.play()
 	if siendo_agarrada:
 		velocity.x = direccion * velocidad
 	else:
 		velocity.x = move_toward(velocity.x, 0, 800 * delta)
 	move_and_slide()
-
 
 #-------------FUNCIONES-------------
 func on_mascara_tiempo_activa():
