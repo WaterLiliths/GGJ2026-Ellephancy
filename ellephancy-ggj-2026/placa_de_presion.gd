3d5e17b394f2b73873d524d403e6686488977056
Marcos Nicolas Salinas <smarcosnicolas@gmail.com>
2026-03-16 09:42:50 -0300

    placa de presion andando

--- ellephancy-ggj-2026/placa_de_presion.gd
+++ ellephancy-ggj-2026/placa_de_presion.gd
@@ -2,15 +2,15 @@ extends Node2D
 
 
 @onready var animated_sprite : AnimatedSprite2D = %AnimatedSprite2D
-@export var tiempo_de_activacion : float = 1.5
+@export var tiempo_de_activacion : float = 1.2
 var activo : bool = false
-var body_adentro : bool = false
+var contador_pisando : int = 0
 var tiempo_body_entered : float = 0
 
 
 func _physics_process(delta: float) -> void:
-	if body_adentro:
-		tiempo_body_entered += delta
+	if contador_pisando > 0: #hay al menos 1 body pisando la placa
+		tiempo_body_entered += delta #empiezp a sumarle tiempo
 		if tiempo_body_entered > tiempo_de_activacion and not activo:
 			set_activo(true)
 	else:
@@ -20,22 +20,24 @@ func _physics_process(delta: float) -> void:
 
 func _on_area_detectar_body_entered(body: Node2D) -> void:
 	if body is Player or body is Empujable:
-		animated_sprite.play("activar")
-		body_adentro = true
+		contador_pisando += 1
 
 
 func _on_area_detectar_body_exited(body: Node2D) -> void:
 	if body is Player or body is Empujable:
-		animated_sprite.play("desactivar")
-		body_adentro = false
-		set_activo(false)
+		contador_pisando -= 1
+		if activo == true and contador_pisando == 0:
+			#print("estaba activo, desactivar")
+			set_activo(false)
 
 
 func set_activo(estado : bool):
 	activo = estado
 	if activo:
-		print("se activo la placa de presion")
+		#print("se activo la placa de presion")
+		animated_sprite.play("activar")
 		%FmodActivarPlaca.play() #placeholder, dsp lo cambiamos :D
 		Global.activar_mecanismo.emit(self)
 	else:
+		animated_sprite.play("desactivar")
 		Global.desactivar_mecanismo.emit(self)
