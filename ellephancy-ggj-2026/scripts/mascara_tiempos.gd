6adfafa63a4aa95d7b792d0f3d33e098f723d6a1
Marcos Nicolas Salinas <smarcosnicolas@gmail.com>
2026-05-07 17:16:15 -0300

    shader nuevo

--- ellephancy-ggj-2026/scripts/mascara_tiempos.gd
+++ ellephancy-ggj-2026/scripts/mascara_tiempos.gd
@@ -4,8 +4,11 @@ extends Node2D #mascara de los tiempos la 1
 @export var id : int = 1
 @export var activa : bool = false
 @onready var color_rect_shader: ColorRect = %ColorRectShader
+@onready var color_rect_shader2_agua: ColorRect = %ColorRectShaderAgua
+
 var material_shader :ShaderMaterial
-var shader_water_color_final = Color(0.0, 0.3214, 0.7775, 1.0)
+var material_shader2 :ShaderMaterial
+var shader_water_color_final = Color(0.0, 0.639, 0.729, 1.0)
 var shader_water_color_inicial = Color(1.0, 1.0, 1.0, 0.003)
 var shader_color_core_inicial = 0.9
 var shader_color_core_final = 0.35
@@ -14,11 +17,17 @@ var shader_backGroundDirX_inicial = 0.01
 var shader_backGroundDirY_inicial = 0.01
 var shader_backGroundDirY_final = 0.75
 
+var shader2_amplitud_inicial := 0.0
+var shader2_amplitud_final := 1.5
+
 
 func _ready() -> void:
 	%ColorRectShader.visible = false
 	material_shader = color_rect_shader.material.duplicate() #primero lo duplico
+	material_shader2 = color_rect_shader2_agua.material.duplicate() #primero lo duplico
 	color_rect_shader.material = material_shader #le asigno el duplicado
+	color_rect_shader2_agua.material = material_shader2 #le asigno el duplicado
+	
 
 func usar():
 	if activa:
@@ -26,8 +35,9 @@ func usar():
 	activa = true
 	Global.mascara_activa = id
 	%ColorRectShader.visible = true
+	color_rect_shader2_agua.visible = true
 	animacion_shader()
-	await get_tree().create_timer(0.2).timeout
+	await get_tree().create_timer(0.3).timeout
 	Global.mascara_tiempo_activa.emit()
 	#efecto_estoy_en_el_pasado(0.2)
 	print("se uso la mascara de tiempos")
@@ -40,8 +50,10 @@ func desactivar():
 		return
 	activa = false
 	animacion_shader()
-	await get_tree().create_timer(0.2).timeout
+	await get_tree().create_timer(0.3).timeout
 	%ColorRectShader.visible = false
+	color_rect_shader2_agua.visible = false
+	
 	Global.mascara_tiempo_desactivar.emit()
 
 
@@ -56,6 +68,9 @@ func shader_efecto_full(duracion):
 	var tween := create_tween()
 	tween.set_trans(Tween.TRANS_SINE)
 	tween.set_ease(Tween.EASE_IN_OUT)
+	#shader nuevo abajo
+	tween.tween_property(material_shader2,"shader_parameter/amplitude",shader2_amplitud_final,duracion)
+	#.---
 	tween.tween_property(material_shader,"shader_parameter/waterColor",shader_water_color_final,duracion)
 	tween.tween_property(material_shader,"shader_parameter/colorCorection",shader_color_core_final,duracion)
 	tween.tween_property(material_shader,"shader_parameter/backGroundDirX",shader_backGroundDirX_final,duracion)
@@ -66,13 +81,16 @@ func shader_efecto_inicial(duracion):
 	var tween := create_tween()
 	tween.set_trans(Tween.TRANS_SINE)
 	tween.set_ease(Tween.EASE_IN_OUT)
+	#shader nuevo
+	tween.tween_property(material_shader2,"shader_parameter/amplitude",shader2_amplitud_inicial,duracion)
+	#---
 	tween.tween_property(material_shader,"shader_parameter/waterColor",shader_water_color_inicial,duracion)
 	tween.tween_property(material_shader,"shader_parameter/colorCorection",shader_color_core_inicial,duracion)
 	tween.tween_property(material_shader,"shader_parameter/backGroundDirX",shader_backGroundDirX_inicial,duracion)
 	tween.tween_property(material_shader,"shader_parameter/backGroundDirY",shader_backGroundDirY_inicial,duracion)
 
 
-func efecto_estoy_en_el_pasado(duracion):
+func efecto_estoy_en_el_pasado(duracion): #si no me equivoco esto no se usa
 	var tween := create_tween()
 	tween.set_trans(Tween.TRANS_SINE)
 	tween.set_ease(Tween.EASE_IN_OUT)
